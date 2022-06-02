3466bea375151e230b273f97a8ca105d1f6670a3
TruxAtCap <simon.truxillo@capgemini.com>
2022-06-02 11:21:14 -0400

    added a watch_k8s_pods.sh at the end of init.sh to see what is going on

--- setup/k8sCluster/watch_k8s_pods.sh
+++ setup/k8sCluster/watch_k8s_pods.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-touch /tmp/watchingpods.tmp
+tmp_file="/tmp/watchingpods.tmp"
+secs=120
 
-while true
+touch $tmp_file
+
+while [ $secs -gt 0 ]
 do
   clear
-  cat /tmp/watchingpods.tmp
-  ssh kube@192.168.1.33 kubectl get po -A -o wide > /tmp/watchingpods.tmp
+  cat $tmp_file
+  ssh kube@192.168.1.33 kubectl get node > $tmp_file
+  echo "" >> $tmp_file
+  ssh kube@192.168.1.33 kubectl get po -A -o wide >> $tmp_file
+  ((secs--))
   sleep 1
-
 done
+
+exit 0
